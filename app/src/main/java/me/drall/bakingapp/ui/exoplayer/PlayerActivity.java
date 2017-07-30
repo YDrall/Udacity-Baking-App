f8f7467ffd7dc58aa8f14aeaecbf399de77979e3
ydrall <yogeshdrall7874@gmail.com>
2017-07-30 14:27:11 +0530

    Change indentation from 2 space to 4 spaces

--- app/src/main/java/me/drall/bakingapp/ui/exoplayer/PlayerActivity.java
+++ app/src/main/java/me/drall/bakingapp/ui/exoplayer/PlayerActivity.java
@@ -54,114 +54,114 @@ import me.drall.bakingapp.R;
  */
 public class PlayerActivity extends AppCompatActivity {
 
-  // bandwidth meter to measure and estimate bandwidth
-  private static final DefaultBandwidthMeter BANDWIDTH_METER = new DefaultBandwidthMeter();
-  private static final String TAG = "PlayerActivity";
+    // bandwidth meter to measure and estimate bandwidth
+    private static final DefaultBandwidthMeter BANDWIDTH_METER = new DefaultBandwidthMeter();
+    private static final String TAG = "PlayerActivity";
 
-  public static final String RECIPE_URL_KEY = "RECIPE_URL_KEY";
+    public static final String RECIPE_URL_KEY = "RECIPE_URL_KEY";
 
-  private SimpleExoPlayer player;
-  private SimpleExoPlayerView playerView;
-  private ComponentListener componentListener;
+    private SimpleExoPlayer player;
+    private SimpleExoPlayerView playerView;
+    private ComponentListener componentListener;
 
-  private String recipeUrl;
+    private String recipeUrl;
 
-  private long playbackPosition;
-  private int currentWindow;
-  private boolean playWhenReady = true;
+    private long playbackPosition;
+    private int currentWindow;
+    private boolean playWhenReady = true;
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_player);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_player);
 
-    recipeUrl = getIntent().getStringExtra(RECIPE_URL_KEY);
+      recipeUrl = getIntent().getStringExtra(RECIPE_URL_KEY);
 
-    componentListener = new ComponentListener();
-    playerView = (SimpleExoPlayerView) findViewById(R.id.video_view);
-  }
-
-  @Override
-  public void onStart() {
-    super.onStart();
-    if (Util.SDK_INT > 23) {
-      initializePlayer();
+      componentListener = new ComponentListener();
+      playerView = (SimpleExoPlayerView) findViewById(R.id.video_view);
     }
-  }
 
-  @Override
-  public void onResume() {
-    super.onResume();
-    hideSystemUi();
-    if ((Util.SDK_INT <= 23 || player == null)) {
-      initializePlayer();
+    @Override
+    public void onStart() {
+      super.onStart();
+      if (Util.SDK_INT > 23) {
+        initializePlayer();
+      }
     }
-  }
 
-  @Override
-  public void onPause() {
-    super.onPause();
-    if (Util.SDK_INT <= 23) {
-      releasePlayer();
+    @Override
+    public void onResume() {
+      super.onResume();
+      hideSystemUi();
+      if ((Util.SDK_INT <= 23 || player == null)) {
+        initializePlayer();
+      }
     }
-  }
 
-  @Override
-  public void onStop() {
-    super.onStop();
-    if (Util.SDK_INT > 23) {
-      releasePlayer();
+    @Override
+    public void onPause() {
+      super.onPause();
+      if (Util.SDK_INT <= 23) {
+        releasePlayer();
+      }
     }
-  }
 
-  private void initializePlayer() {
-    if (player == null) {
-      // a factory to create an AdaptiveVideoTrackSelection
-      TrackSelection.Factory adaptiveTrackSelectionFactory =
-          new AdaptiveTrackSelection.Factory(BANDWIDTH_METER);
-      // using a DefaultTrackSelector with an adaptive video selection factory
-      player = ExoPlayerFactory.newSimpleInstance(new DefaultRenderersFactory(this),
-          new DefaultTrackSelector(adaptiveTrackSelectionFactory), new DefaultLoadControl());
-      player.addListener(componentListener);
-      player.setVideoDebugListener(componentListener);
-      player.setAudioDebugListener(componentListener);
-      playerView.setPlayer(player);
-      player.setPlayWhenReady(playWhenReady);
-      player.seekTo(currentWindow, playbackPosition);
+    @Override
+    public void onStop() {
+      super.onStop();
+      if (Util.SDK_INT > 23) {
+        releasePlayer();
+      }
     }
-    MediaSource mediaSource = buildMediaSource(Uri.parse(recipeUrl));
-    player.prepare(mediaSource, true, false);
-  }
 
-  private void releasePlayer() {
-    if (player != null) {
-      playbackPosition = player.getCurrentPosition();
-      currentWindow = player.getCurrentWindowIndex();
-      playWhenReady = player.getPlayWhenReady();
-      player.removeListener(componentListener);
-      player.setVideoListener(null);
-      player.setVideoDebugListener(null);
-      player.setAudioDebugListener(null);
-      player.release();
-      player = null;
+    private void initializePlayer() {
+      if (player == null) {
+        // a factory to create an AdaptiveVideoTrackSelection
+        TrackSelection.Factory adaptiveTrackSelectionFactory =
+                new AdaptiveTrackSelection.Factory(BANDWIDTH_METER);
+        // using a DefaultTrackSelector with an adaptive video selection factory
+        player = ExoPlayerFactory.newSimpleInstance(new DefaultRenderersFactory(this),
+                new DefaultTrackSelector(adaptiveTrackSelectionFactory), new DefaultLoadControl());
+        player.addListener(componentListener);
+        player.setVideoDebugListener(componentListener);
+        player.setAudioDebugListener(componentListener);
+        playerView.setPlayer(player);
+        player.setPlayWhenReady(playWhenReady);
+        player.seekTo(currentWindow, playbackPosition);
+      }
+      MediaSource mediaSource = buildMediaSource(Uri.parse(recipeUrl));
+      player.prepare(mediaSource, true, false);
     }
-  }
 
-  private MediaSource buildMediaSource(Uri uri) {
-    return new ExtractorMediaSource(uri,
-            new DefaultHttpDataSourceFactory("ua"),
-            new DefaultExtractorsFactory(), null, null);
-  }
+    private void releasePlayer() {
+      if (player != null) {
+        playbackPosition = player.getCurrentPosition();
+        currentWindow = player.getCurrentWindowIndex();
+        playWhenReady = player.getPlayWhenReady();
+        player.removeListener(componentListener);
+        player.setVideoListener(null);
+        player.setVideoDebugListener(null);
+        player.setAudioDebugListener(null);
+        player.release();
+        player = null;
+      }
+    }
 
-  @SuppressLint("InlinedApi")
-  private void hideSystemUi() {
-    playerView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-        | View.SYSTEM_UI_FLAG_FULLSCREEN
-        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-  }
+    private MediaSource buildMediaSource(Uri uri) {
+      return new ExtractorMediaSource(uri,
+              new DefaultHttpDataSourceFactory("ua"),
+              new DefaultExtractorsFactory(), null, null);
+    }
+
+    @SuppressLint("InlinedApi")
+    private void hideSystemUi() {
+      playerView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+              | View.SYSTEM_UI_FLAG_FULLSCREEN
+              | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+              | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+              | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+              | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    }
 
   private class ComponentListener implements ExoPlayer.EventListener, VideoRendererEventListener,
           AudioRendererEventListener {
