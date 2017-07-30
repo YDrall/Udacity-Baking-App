04e653c6e935160683e028a2e94005c6b4c3d8e1
ydrall <yogeshdrall7874@gmail.com>
2017-07-30 14:29:28 +0530

    Change indentation from 2 space to 4 spaces

--- app/src/main/java/me/drall/bakingapp/ui/exoplayer/PlayerActivity.java
+++ app/src/main/java/me/drall/bakingapp/ui/exoplayer/PlayerActivity.java
@@ -72,216 +72,216 @@ public class PlayerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-      super.onCreate(savedInstanceState);
-      setContentView(R.layout.activity_player);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
 
-      recipeUrl = getIntent().getStringExtra(RECIPE_URL_KEY);
+        recipeUrl = getIntent().getStringExtra(RECIPE_URL_KEY);
 
-      componentListener = new ComponentListener();
-      playerView = (SimpleExoPlayerView) findViewById(R.id.video_view);
+        componentListener = new ComponentListener();
+        playerView = (SimpleExoPlayerView) findViewById(R.id.video_view);
     }
 
     @Override
     public void onStart() {
-      super.onStart();
-      if (Util.SDK_INT > 23) {
-        initializePlayer();
-      }
+        super.onStart();
+        if (Util.SDK_INT > 23) {
+            initializePlayer();
+        }
     }
 
     @Override
     public void onResume() {
-      super.onResume();
-      hideSystemUi();
-      if ((Util.SDK_INT <= 23 || player == null)) {
-        initializePlayer();
-      }
+        super.onResume();
+        hideSystemUi();
+        if ((Util.SDK_INT <= 23 || player == null)) {
+            initializePlayer();
+        }
     }
 
     @Override
     public void onPause() {
-      super.onPause();
-      if (Util.SDK_INT <= 23) {
-        releasePlayer();
-      }
+        super.onPause();
+        if (Util.SDK_INT <= 23) {
+            releasePlayer();
+        }
     }
 
     @Override
     public void onStop() {
-      super.onStop();
-      if (Util.SDK_INT > 23) {
-        releasePlayer();
-      }
+        super.onStop();
+        if (Util.SDK_INT > 23) {
+            releasePlayer();
+        }
     }
 
     private void initializePlayer() {
-      if (player == null) {
-        // a factory to create an AdaptiveVideoTrackSelection
-        TrackSelection.Factory adaptiveTrackSelectionFactory =
-                new AdaptiveTrackSelection.Factory(BANDWIDTH_METER);
-        // using a DefaultTrackSelector with an adaptive video selection factory
-        player = ExoPlayerFactory.newSimpleInstance(new DefaultRenderersFactory(this),
-                new DefaultTrackSelector(adaptiveTrackSelectionFactory), new DefaultLoadControl());
-        player.addListener(componentListener);
-        player.setVideoDebugListener(componentListener);
-        player.setAudioDebugListener(componentListener);
-        playerView.setPlayer(player);
-        player.setPlayWhenReady(playWhenReady);
-        player.seekTo(currentWindow, playbackPosition);
-      }
-      MediaSource mediaSource = buildMediaSource(Uri.parse(recipeUrl));
-      player.prepare(mediaSource, true, false);
+        if (player == null) {
+            // a factory to create an AdaptiveVideoTrackSelection
+            TrackSelection.Factory adaptiveTrackSelectionFactory =
+                    new AdaptiveTrackSelection.Factory(BANDWIDTH_METER);
+            // using a DefaultTrackSelector with an adaptive video selection factory
+            player = ExoPlayerFactory.newSimpleInstance(new DefaultRenderersFactory(this),
+                    new DefaultTrackSelector(adaptiveTrackSelectionFactory), new DefaultLoadControl());
+            player.addListener(componentListener);
+            player.setVideoDebugListener(componentListener);
+            player.setAudioDebugListener(componentListener);
+            playerView.setPlayer(player);
+            player.setPlayWhenReady(playWhenReady);
+            player.seekTo(currentWindow, playbackPosition);
+        }
+        MediaSource mediaSource = buildMediaSource(Uri.parse(recipeUrl));
+        player.prepare(mediaSource, true, false);
     }
 
     private void releasePlayer() {
-      if (player != null) {
-        playbackPosition = player.getCurrentPosition();
-        currentWindow = player.getCurrentWindowIndex();
-        playWhenReady = player.getPlayWhenReady();
-        player.removeListener(componentListener);
-        player.setVideoListener(null);
-        player.setVideoDebugListener(null);
-        player.setAudioDebugListener(null);
-        player.release();
-        player = null;
-      }
+        if (player != null) {
+            playbackPosition = player.getCurrentPosition();
+            currentWindow = player.getCurrentWindowIndex();
+            playWhenReady = player.getPlayWhenReady();
+            player.removeListener(componentListener);
+            player.setVideoListener(null);
+            player.setVideoDebugListener(null);
+            player.setAudioDebugListener(null);
+            player.release();
+            player = null;
+        }
     }
 
     private MediaSource buildMediaSource(Uri uri) {
-      return new ExtractorMediaSource(uri,
-              new DefaultHttpDataSourceFactory("ua"),
-              new DefaultExtractorsFactory(), null, null);
+        return new ExtractorMediaSource(uri,
+                new DefaultHttpDataSourceFactory("ua"),
+                new DefaultExtractorsFactory(), null, null);
     }
 
     @SuppressLint("InlinedApi")
     private void hideSystemUi() {
-      playerView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-              | View.SYSTEM_UI_FLAG_FULLSCREEN
-              | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-              | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-              | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-              | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        playerView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
     }
 
-  private class ComponentListener implements ExoPlayer.EventListener, VideoRendererEventListener,
-          AudioRendererEventListener {
-    @Override
-    public void onTimelineChanged(Timeline timeline, Object manifest) {
-      // Do nothing.
+    private class ComponentListener implements ExoPlayer.EventListener, VideoRendererEventListener,
+            AudioRendererEventListener {
+        @Override
+        public void onTimelineChanged(Timeline timeline, Object manifest) {
+            // Do nothing.
+        }
+
+        @Override
+        public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
+            // Do nothing.
+        }
+
+        @Override
+        public void onLoadingChanged(boolean isLoading) {
+            // Do nothing.
+        }
+
+        @Override
+        public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+            String stateString;
+            switch (playbackState) {
+                case ExoPlayer.STATE_IDLE:
+                    stateString = "ExoPlayer.STATE_IDLE      -";
+                    break;
+                case ExoPlayer.STATE_BUFFERING:
+                    stateString = "ExoPlayer.STATE_BUFFERING -";
+                    break;
+                case ExoPlayer.STATE_READY:
+                    stateString = "ExoPlayer.STATE_READY     -";
+                    break;
+                case ExoPlayer.STATE_ENDED:
+                    stateString = "ExoPlayer.STATE_ENDED     -";
+                    break;
+                default:
+                    stateString = "UNKNOWN_STATE             -";
+                    break;
+            }
+            Log.d(TAG, "changed state to " + stateString + " playWhenReady: " + playWhenReady);
+        }
+
+        @Override
+        public void onPlayerError(ExoPlaybackException error) {
+            // Do nothing.
+        }
+
+        @Override
+        public void onPositionDiscontinuity() {
+            // Do nothing.
+        }
+
+        @Override
+        public void onPlaybackParametersChanged(PlaybackParameters playbackParameters) {
+            // Do nothing.
+        }
+
+        @Override
+        public void onVideoEnabled(DecoderCounters counters) {
+
+        }
+
+        @Override
+        public void onVideoDecoderInitialized(String decoderName, long initializedTimestampMs, long initializationDurationMs) {
+
+        }
+
+        @Override
+        public void onVideoInputFormatChanged(Format format) {
+
+        }
+
+        @Override
+        public void onDroppedFrames(int count, long elapsedMs) {
+
+        }
+
+        @Override
+        public void onVideoSizeChanged(int width, int height, int unappliedRotationDegrees, float pixelWidthHeightRatio) {
+
+        }
+
+        @Override
+        public void onRenderedFirstFrame(Surface surface) {
+
+        }
+
+        @Override
+        public void onVideoDisabled(DecoderCounters counters) {
+
+        }
+
+        @Override
+        public void onAudioEnabled(DecoderCounters counters) {
+
+        }
+
+        @Override
+        public void onAudioSessionId(int audioSessionId) {
+
+        }
+
+        @Override
+        public void onAudioDecoderInitialized(String decoderName, long initializedTimestampMs, long initializationDurationMs) {
+
+        }
+
+        @Override
+        public void onAudioInputFormatChanged(Format format) {
+
+        }
+
+        @Override
+        public void onAudioTrackUnderrun(int bufferSize, long bufferSizeMs, long elapsedSinceLastFeedMs) {
+
+        }
+
+        @Override
+        public void onAudioDisabled(DecoderCounters counters) {
+
+        }
     }
-
-    @Override
-    public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
-      // Do nothing.
-    }
-
-    @Override
-    public void onLoadingChanged(boolean isLoading) {
-      // Do nothing.
-    }
-
-    @Override
-    public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
-      String stateString;
-      switch (playbackState) {
-        case ExoPlayer.STATE_IDLE:
-          stateString = "ExoPlayer.STATE_IDLE      -";
-          break;
-        case ExoPlayer.STATE_BUFFERING:
-          stateString = "ExoPlayer.STATE_BUFFERING -";
-          break;
-        case ExoPlayer.STATE_READY:
-          stateString = "ExoPlayer.STATE_READY     -";
-          break;
-        case ExoPlayer.STATE_ENDED:
-          stateString = "ExoPlayer.STATE_ENDED     -";
-          break;
-        default:
-          stateString = "UNKNOWN_STATE             -";
-          break;
-      }
-      Log.d(TAG, "changed state to " + stateString + " playWhenReady: " + playWhenReady);
-    }
-
-    @Override
-    public void onPlayerError(ExoPlaybackException error) {
-      // Do nothing.
-    }
-
-    @Override
-    public void onPositionDiscontinuity() {
-      // Do nothing.
-    }
-
-    @Override
-    public void onPlaybackParametersChanged(PlaybackParameters playbackParameters) {
-      // Do nothing.
-    }
-
-    @Override
-    public void onVideoEnabled(DecoderCounters counters) {
-
-    }
-
-    @Override
-    public void onVideoDecoderInitialized(String decoderName, long initializedTimestampMs, long initializationDurationMs) {
-
-    }
-
-    @Override
-    public void onVideoInputFormatChanged(Format format) {
-
-    }
-
-    @Override
-    public void onDroppedFrames(int count, long elapsedMs) {
-
-    }
-
-    @Override
-    public void onVideoSizeChanged(int width, int height, int unappliedRotationDegrees, float pixelWidthHeightRatio) {
-
-    }
-
-    @Override
-    public void onRenderedFirstFrame(Surface surface) {
-
-    }
-
-    @Override
-    public void onVideoDisabled(DecoderCounters counters) {
-
-    }
-
-    @Override
-    public void onAudioEnabled(DecoderCounters counters) {
-
-    }
-
-    @Override
-    public void onAudioSessionId(int audioSessionId) {
-
-    }
-
-    @Override
-    public void onAudioDecoderInitialized(String decoderName, long initializedTimestampMs, long initializationDurationMs) {
-
-    }
-
-    @Override
-    public void onAudioInputFormatChanged(Format format) {
-
-    }
-
-    @Override
-    public void onAudioTrackUnderrun(int bufferSize, long bufferSizeMs, long elapsedSinceLastFeedMs) {
-
-    }
-
-    @Override
-    public void onAudioDisabled(DecoderCounters counters) {
-
-    }
-  }
 
 }
